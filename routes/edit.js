3d7ddaf0c0dc050ab9ea2ca39a7d4012567d70ef
calimendoza <mbaivictor1@gmail.com>
2023-01-16 00:42:01 -0800

    fixed bug

--- routes/edit.js
+++ routes/edit.js
@@ -2,10 +2,9 @@ const express = require('express')
 const router = express.Router()
 const editController = require('../controllers/edit')
 
-router.get('/:id', editController.getEdit) //get edit page with id of item we want to edit
-router.post('/remove/:id', editController.deleteItem) //deletes Item
-
-router.post('/update/:id', editController.updateItem)// updates the item usin data passed from the form.
+router.get('/:id', editController.getEdit)//get edit page with id of item we want to edit
+router.get('/remove/:id', editController.deleteItem) //deletes Item
+router.post('/update/:id', editController.updateItem) // updates the item usin data passed from the form.
 
 
 module.exports = router
